d7bdb7ebda0a89be4c96b7afc2a404ea2abe5492
ThanhTC21 <thanhtc21@fpt.com.vn>
2022-07-25 14:48:52 +0700

    update

--- js/slideshowImage.js
+++ js/slideshowImage.js
@@ -9,7 +9,7 @@ setInterval(nextImage, nextImageDelay);
 
 function nextImage() {
   // slideshowImages[currentImageCounter].style.display = "none";
-  slideshowImages[currentImageCounter].style.opacity = 0;
+  // slideshowImages[currentImageCounter].style.opacity = 0;
 
   currentImageCounter = (currentImageCounter+1) % slideshowImages.length;
 
